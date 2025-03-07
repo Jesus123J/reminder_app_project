299255bf035a9a9af3c2dae14332e82b61d1ccb2
Thiago <gutierrezdelatorrej30@gmail.com>
2025-03-07 15:03:59 -0500

    structure implemented

---
+++ reminder-app/src/test/java/com/reminder/app/controller/ControllerDashboard.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.reminder.app.controller;
+
+/**
+ *
+ * @author Jesus Gutierrez
+ */
+public class ControllerDashboard {
+    
+    
+    
+}
---
+++ reminder-app/src/test/java/com/reminder/app/model/ModelDataDocument.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.reminder.app.model;
+
+/**
+ *
+ * @author Jesus Gutierrez
+ */
+public class ModelDataDocument {
+    
+}
